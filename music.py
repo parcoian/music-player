36d44aade9d5f5f10d655ce1da0395cbf62d7fa6
Ian Parco <93970987+parcoian@users.noreply.github.com>
2022-01-17 20:21:49 +0800

    Spotiplay Music Player PL Finals

--- music.py
+++ music.py
@@ -33,9 +33,6 @@ def add_track():
         track = track.replace(".mp3", "")
         music_box.insert(END, track)
 
-    # for track in tracks:
-    #   music_box.insert(END,track)
-
 #Removing single/multiple tracks function
 def remove_track():
 
@@ -63,8 +60,6 @@ def play():
     #Getting the title of the song and directory of the mp3
     tracks = music_box.get(ACTIVE)
     tracks = f'C:/Users/Nero/music-player/tracks/{tracks}.mp3'
-    # trackdir = os.path.dirname('C:/Users/Nero/music player/tracks')
-    # trackpath = os.path.join(trackdir,".mp3")
 
     #Playing the track
     pygame.mixer.music.load(tracks)
@@ -72,12 +67,6 @@ def play():
 
     #Calling of track_time function
     track_time()
-
-    # position = int(length_track)
-    # track_slide.config(to=position, value=0)
-    
-    # curr_volume = pygame.mixer.music.get_volume
-    # track_slide_label.config(text=curr_volume*100)
 
 #Pause Function
 global paused
@@ -169,9 +158,6 @@ def track_time():
     #Getting the Current track time
     curr_time = pygame.mixer.music.get_pos()/1000
 
-    #label to get the data
-    #track_slide_label.config(text=f'Slider:{int(track_slide.get())} and track_pos:{int(curr_time)}')
-
     #Converting track length to time format
     current_converted_track = time.strftime('%M:%S', time.gmtime(curr_time))
 
@@ -211,37 +197,20 @@ def track_time():
         time_nav = int(track_slide.get())+1
         track_slide.config(value=time_nav)
 
-
-    #Song time in time bar
-    #time_bar.config(text=f'{current_length_track}')
-    #The slider position value of the current playing track
-    #track_slide.config(value=int(curr_time))
-
-    # curr_time_label = Label(body, text=int(curr_time))
-    # curr_time_label.pack(pady=10)
-
-    #Track slider position
-    
     #Play time of the track
     time_bar.after(1000, track_time)
 
-
 #Track navigator slider
 def t_slider():
-    #track_slide_label.config(text=f'{int(track_slide.get())} of {int({length_track})}')
     
     track = music_box.get(ACTIVE)
     track = f'C:/Users/Nero/music-player/tracks/{track}.mp3'
-    
     pygame.mixer.music.load(track)
     pygame.mixer.music.play(loops=0, start=int(track_slide.get()))
-
 
 #Track Volume Function
 def t_vol():
     pygame.mixer.music.set_volume(vol_slider.get())
-    # curr_volume = pygame.mixer.music.get_volume
-    # track_slide_label.config(text=curr_volume*100)
 
 
 #________________MAIN GUI_____________________
@@ -251,14 +220,12 @@ main_frame = Frame(body)
 main_frame.pack(pady=30)
 
 #Music Box, the list of the music that has been import
-music_box = Listbox(main_frame, bg="#FAF1E6", fg="#064420", width=55)
+music_box = tk.Listbox(main_frame, bg="#FAF1E6", fg="#064420", width=55)
 music_box.grid(row=0, column=0)
 
 #Music track navigator slider
 track_slide = ttk.Scale(main_frame, from_=0, to=100, orient=HORIZONTAL, value=0, command = t_slider, length=330)
 track_slide.grid(row=1, column=0, pady=20)
-# track_slide_label = Label(body, text="0")
-# track_slide_label.pack(pady=10)
 
 #Configuration of the Buttons
 controls_frame = Frame(main_frame, bg="#E4EFE7")
